937cb05c0370440da8cc4ee4e09bf755ae118047
KameronKeller <mrkameronkeller@gmail.com>
2024-04-13 14:42:27 -0700

    Update endpoints

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ let businesses = {};
 let currentBusinessId = 0;
 let reviews = {};
 let reviewId = 0;
+let photos = {};
+let currentphotoId = 0;
 
 function addReview(review) {
   let businessId = review.businessId;
@@ -20,15 +22,24 @@ function addReview(review) {
   }
 }
 
+function addPhoto(photo) {
+  let businessId = photo.businessId;
+  if (businessId in photos) {
+    photos[businessId].push(photo);
+  } else {
+    photos[businessId] = [photo];
+  }
+}
+
 function on_ready() {
   console.log("server ready!");
 }
 
-function isNotValidBusinessId(businessId) {
-  return !businesses.hasOwnProperty(businessId);
+function isNotValidId(dataObject, id) {
+  return !dataObject.hasOwnProperty(id);
 }
 
-function hasRequiredProperties(requestBody) {
+function hasRequiredBusinessProperties(requestBody) {
   return (
     requestBody.hasOwnProperty("name") &&
     requestBody.hasOwnProperty("streetAddress") &&
@@ -41,13 +52,28 @@ function hasRequiredProperties(requestBody) {
   );
 }
 
+function hasRequiredReviewProperties(requestBody) {
+  return (
+    requestBody.hasOwnProperty("businessId") &&
+    requestBody.hasOwnProperty("stars") &&
+    requestBody.hasOwnProperty("expense")
+  );
+}
+
+function hasRequiredPhotoProperties(requestBody) {
+  return (
+    requestBody.hasOwnProperty("businessId") &&
+    requestBody.hasOwnProperty("photoUrl")
+  );
+}
+
 app.listen(port, () => {
   on_ready();
 });
 
 // Request: POST /businesses - create a business
 app.post("/businesses", (req, res) => {
-  if (hasRequiredProperties(req.body)) {
+  if (hasRequiredBusinessProperties(req.body)) {
     currentBusinessId++;
     businesses[currentBusinessId] = req.body;
     res.send({
@@ -62,9 +88,9 @@ app.post("/businesses", (req, res) => {
 // Request: PUT /businesses/{businessId} - update a business
 app.put("/businesses/:businessId", (req, res) => {
   const businessId = req.params.businessId;
-  if (isNotValidBusinessId(businessId)) {
+  if (isNotValidId(businesses, businessId)) {
     res.status(400).send("Error 400: Specified resource does not exist");
-  } else if (hasRequiredProperties(req.body)) {
+  } else if (hasRequiredBusinessProperties(req.body)) {
     businesses[businessId] = req.body;
     res.send({
       businessId: businessId,
@@ -78,7 +104,7 @@ app.put("/businesses/:businessId", (req, res) => {
 // Request: DELETE /businesses/{id} - delete a business
 app.delete("/businesses/:businessId", (req, res) => {
   const businessId = req.params.businessId;
-  if (isNotValidBusinessId(businessId)) {
+  if (isNotValidId(businesses, businessId)) {
     res.status(400).send("Error 400: Specified resource does not exist");
   } else {
     delete businesses.businessId;
@@ -94,60 +120,92 @@ app.get("/businesses", (req, res) => {
 // Request: GET /businesses/{businessId} - list a specific business's details
 app.get("/businesses/:businessId", (req, res) => {
   const businessId = req.params.businessId;
-  if (isNotValidBusinessId(businessId)) {
+  if (isNotValidId(businesses, businessId)) {
     res.status(400).send("Error 400: Specified resource does not exist");
   } else {
     let business = businesses[businessId];
     business["reviews"] = reviews[businessId];
+    business["photos"] = photos[businessId];
     res.send(business);
   }
 });
 
 // Request: POST /reviews - create a review of a business
 app.post("/reviews", (req, res) => {
-  if (
-    req.body.hasOwnProperty("businessId") &&
-    req.body.hasOwnProperty("stars") &&
-    req.body.hasOwnProperty("expense")
-  ) {
+  if (hasRequiredReviewProperties(req.body)) {
+    reviewId++;
     addReview(req.body);
     res.send({
       reviewId: reviewId,
     });
-    reviewId += 1;
   } else {
     res.status(400).send("Error 400: Missing necessary fields");
   }
 });
 
 // Request: PUT /reviews/{reviewId} - update a review of a business
-// app.put("/reviews", (req, res) => {
-//     res.send()
-// })
+app.put("/reviews/:reviewId", (req, res) => {
+  const reviewId = req.params.reviewId;
+  if (isNotValidId(reviews, reviewId)) {
+    res.status(400).send("Error 400: Specified resource does not exist");
+  } else if (hasRequiredReviewProperties(req.body)) {
+    reviews[reviewId] = req.body;
+    res.send({
+      reviewId: reviewId,
+    });
+  } else {
+    res.status(400).send("Error 400: Missing necessary fields");
+  }
+});
 
 // Request: GET /reviews - list all of a user's reviews
-app.get("", (req, res) => {
-  res.send();
+app.get("/reviews", (req, res) => {
+  res.send(reviews);
 });
 
 // Request: POST /photos - add a photo of a business
-app.post("", (req, res) => {
-  res.send();
+app.post("/photos", (req, res) => {
+  if (hasRequiredPhotoProperties(req.body)) {
+    currentphotoId++;
+    addPhoto(req.body);
+    res.send({
+      photoId: currentphotoId,
+    });
+    console.log("== photos", photos);
+  } else {
+    res.status(400).send("Error 400: Missing necessary fields");
+  }
 });
 
 // Request: DELETE /photos/{photoId} - delete a photo
-app.delete("", (req, res) => {
-  res.send();
+app.delete("/photos/:photoId", (req, res) => {
+  const photoId = req.params.photoId;
+  if (isNotValidId(photos, photoId)) {
+    res.status(400).send("Error 400: Specified resource does not exist");
+  } else {
+    delete photos.photoId;
+    res.sendStatus(200);
+  }
 });
 
 // Request: PUT /photos/{photoId} - update a photo caption
-app.put("", (req, res) => {
-  res.send();
+app.put("/photos/:photoId", (req, res) => {
+  const photoId = req.params.photoId;
+  if (isNotValidId(photos, photoId)) {
+    res.status(400).send("Error 400: Specified resource does not exist");
+  } else if (hasRequiredPhotoProperties(req.body)) {
+    photos[photoId] = req.body;
+    res.send({
+      photoId: photoId,
+    });
+  } else {
+    res.status(400).send("Error 400: Missing necessary fields");
+  }
 });
 
 // Request: GET /photos - retrieve photos a user has uploaded
-app.get("", (req, res) => {
-  res.send();
+app.get("/photos", (req, res) => {
+  res.send(photos);
 });
 
 app.use("*", function (req, res) {
@@ -155,51 +213,3 @@ app.use("*", function (req, res) {
     err: "The requested resource doesn't exist",
   });
 });
-
-/*************************************/
-
-// app.delete("", (req, res, next) => {
-//     res.send()
-// })
-
-// app.put("", (req, res, next) => {
-//     res.send()
-// })
-
-// app.post("", (req, res, next) => {
-//     res.send()
-// })
-
-// app.get("", (req, res, next) => {
-//     res.send()
-// })
-
-// // Handle certain API endpoints
-// app.get("/messages", (req, res, next) => {
-//     res.send(messages)
-// })
-
-// app.post("/messages", (req, res, next) => {
-//     const index = messages.length
-//     messages.push(req.body)
-//     console.log(req.body)
-//     res.send({
-//         "index": index,
-//         "links": {
-//             "message": `/messages/${index}`
-//         }
-//     })
-// })
-
-// app.get("/messages/:index", (req, res, next) => {
-//     const index = req.params.index
-
-//     if (index < 0 || index >= messages.length) {
-//         res.status(404).send({"error": `Message ${index} not found`});
-
-//         // Do what is next in expresses chain of functions
-//         next();
-//     }
-
-//     res.send(messages[req.params.index])
-// })
